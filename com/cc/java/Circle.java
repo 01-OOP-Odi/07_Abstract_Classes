687fe5d21e87b5198b9ad5764ea66b22d609af66
Your Name <you@example.com>
2023-05-30 15:34:22 +0200

    Sync Test

--- com/cc/java/Circle.java
+++ com/cc/java/Circle.java
@@ -15,6 +15,7 @@ public class Circle extends Shape{
     @Override
     public double area() {
         // return Math.PI * radius * radius;
+        
         return Math.PI * Math.pow(radius, 2);
     }
 
